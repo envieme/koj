9426f79a7dd88e6f6e23c7703308343c4de5061c
envieme <envieme@gmail.com>
2024-07-04 14:38:41 +0700

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export const runtime = 'edge';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      {/* Add additional content or navigation here */}
+    </div>
+  );
+};
+
+export default NotFoundPage;
