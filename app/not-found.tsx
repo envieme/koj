7fd991384143a3d1f8e67e1e3d46cb3ecae6a1d9
envieme <envieme@gmail.com>
2024-07-04 15:50:57 +0700

    Update not-found.tsx
    Changed Not Found Function

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,6 @@ import Link from 'next/link'
 import { headers } from 'next/headers'
  
 export default async function NotFound() {
-  const headersList = headers()
-  const domain = headersList.get('host')
-  const data = await getSiteData(domain)
   return (
     <div>
       <h2>Not Found: {data.name}</h2>
