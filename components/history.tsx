c142b546d64871c3ce27bce0a843c8577440b4f1
envieme <envieme@gmail.com>
2024-07-04 15:35:20 +0700

    Update history.tsx
    Changed < icon for History to 56px from 16px

--- components/history.tsx
+++ components/history.tsx
@@ -46,7 +46,7 @@ export function History({ location, children }: HistoryProps) {
           })}
           disabled={isGenerating}
         >
-          {location === 'header' ? <Menu /> : <ChevronLeft size={16} />}
+          {location === 'header' ? <Menu /> : <ChevronLeft size={56} />}
         </Button>
       </SheetTrigger>
       <SheetContent className="w-64 rounded-tl-xl rounded-bl-xl">
