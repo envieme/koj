ac3a319765db7e4d99744624b2b7fc0d28909f86
envieme <envieme@gmail.com>
2024-07-04 13:42:16 +0700

    Update page.tsx
    export const runtime = 'edge';

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Chat } from '@/components/chat'
 import { generateId } from 'ai'
 import { AI } from './actions'
-
+export const runtime = 'edge';
 export const maxDuration = 60
 
 export default function Page() {
