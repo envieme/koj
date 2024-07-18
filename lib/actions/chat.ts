6913bc946b441143bedc8fd853f59decd2e59535
envieme <envieme@gmail.com>
2024-07-18 17:54:14 +0700

    Update chat.ts

--- lib/actions/chat.ts
+++ lib/actions/chat.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { type Chat } from '@/lib/types'
-import { Redis } from '@upstash/redis'
+import { Redis } from '@upstash/redis/cloudflare'
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || '',
