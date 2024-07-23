d5a5c9ade17c8e85cb59d3cf05e2c83fb43acab5
envieme <envieme@gmail.com>
2024-07-23 17:24:10 +0700

    Update query-suggestor.tsx

--- lib/agents/query-suggestor.tsx
+++ lib/agents/query-suggestor.tsx
@@ -21,7 +21,7 @@ export async function querySuggestor(
   let finalRelatedQueries: PartialRelated = {}
   await streamObject({
     model: getModel(),
-    system: `As a professional web researcher, your task is to generate a set of three queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
+    system: `As a professional news researcher, your task is to generate a set of three queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
 
     For instance, if the original query was "Starship's third test flight key milestones", your output should follow this format:
 
